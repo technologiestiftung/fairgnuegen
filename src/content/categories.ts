f79f0fc0bc0f158ec51d78d0788d089b9f2cfac1
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-09-04 08:56:06 +0200

    feat: support for categories, chore: refactor

---
+++ src/content/categories.ts
@@ -0,0 +1,58 @@
+export type CategoryIdentifier =
+	| "kultur"
+	| "freizeit"
+	| "bildung_beratung"
+	| "sport"
+	| "all";
+
+export type CategoryDetails = {
+	color: string;
+	name: string;
+	image: string;
+	description: string;
+	isRendered: boolean;
+};
+
+export type CategoryMap = {
+	[K in CategoryIdentifier]: CategoryDetails;
+};
+
+export const categoryMap: CategoryMap = {
+	kultur: {
+		color: "bg-culture",
+		name: "Kultur",
+		image: "/kultur.jpg",
+		description: "Angebote fürs Museum, Theater, Konzerte und vieles mehr",
+		isRendered: true,
+	},
+	sport: {
+		color: "bg-sport",
+		name: "Sport",
+		image: "/sport.jpg",
+		description:
+			"Angebote um körperlich fit zu bleiben oder den Sport zu feiern",
+		isRendered: true,
+	},
+	bildung_beratung: {
+		color: "bg-education",
+		name: "Bildung & Beratung",
+		image: "/bildung.jpg",
+		description: "Weiterbildungs- und Beratungsangebote",
+		isRendered: true,
+	},
+	freizeit: {
+		color: "bg-leisure",
+		name: "Freizeit",
+		image: "/freizeit.jpg",
+		description:
+			"Angebote fürs Kino, Restaurants, Bars und Aktivitäten an der freien Luft",
+		isRendered: true,
+	},
+	all: {
+		color: "bg-primary-blue",
+		name: "Alle Angebote",
+		image: "",
+		description: "",
+		isRendered: false,
+	},
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,33 +4,34 @@ import SearchBar from "../components/search-bar/search-bar";
 import { Layout } from "../layout/layout";
 import ShowAllButton from "../components/buttons/show-all-button";
 import ArrowRightIcon from "../components/icons/arrow-right-icon";
+import { CategoryDetails, categoryMap } from "../content/categories";
 
 interface HeroCardProps {
-	title: string;
-	description: string;
-	image: string;
-	color: string;
+	identifier: string;
+	category: CategoryDetails;
 }
 
-export const HeroCard: React.FC<HeroCardProps> = ({
-	title,
-	description,
-	image,
-	color,
-}) => {
+export const HeroCard: React.FC<HeroCardProps> = ({ identifier, category }) => {
 	return (
 		<div className="h-56 flex flex-row gap-0 shadow-xl w-full border">
-			<div className={`${color} h-full w-[5%]`}></div>
+			<div className={`${category.color} h-full w-[5%]`}></div>
 			<div className="w-[45%]">
-				<img src={image} alt="" className="object-cover h-full w-full" />
+				<img
+					src={category.image}
+					alt=""
+					className="object-cover h-full w-full"
+				/>
 			</div>
 			<div className="flex flex-col justify-between p-4 w-[50%]">
-				<div className="text-xl font-bold">{title}</div>
-				<div>{description}</div>
-				<button className="text-primary-blue text-left flex flex-row items-center justify-start gap-2">
+				<div className="text-xl font-bold">{category.name}</div>
+				<div>{category.description}</div>
+				<a
+					href={`/all-offers/?category=${identifier}`}
+					className="text-primary-blue text-left flex flex-row items-center justify-start gap-2"
+				>
 					<div>entdecken</div>
 					<ArrowRightIcon></ArrowRightIcon>
-				</button>
+				</a>
 			</div>
 		</div>
 	);
@@ -55,30 +56,11 @@ export default function Index() {
 
 				<div className="w-full border-b border-separator mb-12 mt-2"></div>
 				<div className="w-full grid grid-cols-1 grid-rows-4 gap-x-8 gap-y-8 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-32 sm:gap-y-16 px-4 sm:px-0">
-					<HeroCard
-						title="Kultur"
-						description="Angebote fürs Museum, Theater, Konzerte und vieles mehr"
-						image="kultur.jpg"
-						color="bg-culture"
-					></HeroCard>
-					<HeroCard
-						title="Sport"
-						description="Angebote um körperlich fit zu bleiben oder den Sport zu feiern"
-						image="sport.jpg"
-						color="bg-sport"
-					></HeroCard>
-					<HeroCard
-						title="Beratung & Bildung"
-						description="Weiterbildungs- und Beratungsangebote"
-						image="bildung.jpg"
-						color="bg-education"
-					></HeroCard>
-					<HeroCard
-						title="Freizeit"
-						description="Angebote fürs Kino, Restaurants, Bars und Aktivitäten an der freien Luft"
-						image="freizeit.jpg"
-						color="bg-leisure"
-					></HeroCard>
+					{Object.entries(categoryMap)
+						.filter(([, category]) => category.isRendered)
+						.map(([key, category]) => (
+							<HeroCard key={key} identifier={key} category={category} />
+						))}
 				</div>
 				<div className="w-full border-b border-separator mb-12 mt-12"></div>
 				<div className="w-full flex flex-row justify-end px-4 sm:px-0">
