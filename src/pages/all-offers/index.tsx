09663a4581ec3e6bef204e1538d97d4cb03d1faf
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-09-04 09:30:41 +0200

    feat: support for categories, chore: refactor (#10)
    * feat: support for categories, chore: refactor
    
    * feat: search on landing page
    
    * feat: teaser for further categories

--- src/pages/all-offers/index.tsx
+++ src/pages/all-offers/index.tsx
@@ -9,10 +9,13 @@ import SearchBar from "../../components/search-bar/search-bar";
 import { offers } from "../../content/content";
 import { Layout } from "../../layout/layout";
 import BackButton from "../../components/buttons/back-button";
+import { CategoryIdentifier, categoryMap } from "../../content/categories";
+import { CategoryCard } from "../../components/categories/category-card";
 
 export default function Index() {
 	const [searchParams, setSearchParams] = useSearchParams();
-	const category = searchParams.get("category");
+	const category = (searchParams.get("category") ??
+		"all") as CategoryIdentifier;
 	const [search, setSearch] = useState(searchParams.get("search"));
 	const [showFreeOffersOnly, setShowFreeOffersOnly] = useState(
 		searchParams.get("free") === "true",
@@ -26,12 +29,16 @@ export default function Index() {
 			sort: sortAscending ? "asc" : "desc",
 			search: search ?? "",
 			free: showFreeOffersOnly ? "true" : "false",
+			category: category ?? "",
 		});
 	}, [search, sortAscending, showFreeOffersOnly]);
 
 	const filteredOffers = useMemo(() => {
 		const filtered = offers
-			.filter((o) => !category || o.category.includes(category))
+			.filter(
+				(o) =>
+					category === "all" || o.category.includes(categoryMap[category].name),
+			)
 			.filter(
 				(o) =>
 					!search || o.provider.toLowerCase().includes(search.toLowerCase()),
@@ -49,9 +56,19 @@ export default function Index() {
 	return (
 		<Layout>
 			<div>
-				<div className="w-full bg-primary-blue flex flex-row justify-center items-center text-[#ffffff] p-3 mb-10 font-bold text-xl">
-					Alle Angebote
+				{category !== "all" && (
+					<img
+						src={categoryMap[category].image}
+						alt={categoryMap[category].name}
+						className="hidden sm:block w-full h-[300px] object-cover"
+					/>
+				)}
+				<div
+					className={`w-full ${categoryMap[category].color ?? "bg-primary-blue"} flex flex-row justify-center items-center text-[#ffffff] p-3 mb-10 font-bold text-xl`}
+				>
+					{category !== "all" ? categoryMap[category].name : "Alle Angebote"}
 				</div>
+
 				<div className="max-w-3xl mx-auto flex flex-col">
 					<div className="mx-4 sm:mx-0">
 						<div className="flex flex-col mb-10">
@@ -81,6 +98,7 @@ export default function Index() {
 							<RocketIcon></RocketIcon>
 							<p className="text-md text-primary-blue">
 								{filteredOffers.length} Angebote gefunden
+								{category !== "all" && ` für "${categoryMap[category].name}"`}
 							</p>
 						</div>
 					</div>
@@ -97,6 +115,21 @@ export default function Index() {
 					</div>
 
 					<BackButton onClick={() => {}}></BackButton>
+
+					{category !== "all" && (
+						<div className="my-8 mx-4 sm:mx-0">
+							<div className="text-xl font-bold my-4">
+								Entdecke weitere Kategorien
+							</div>
+							<div className="w-full grid grid-cols-1 grid-rows-3 sm:grid-cols-3 sm:grid-rows-1 gap-4 ">
+								{Object.entries(categoryMap)
+									.filter(([key, c]) => c.isRendered && key !== category)
+									.map(([key, c]) => (
+										<CategoryCard key={key} identifier={key} category={c} />
+									))}
+							</div>
+						</div>
+					)}
 				</div>
 			</div>
 		</Layout>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,13 @@
-import React from "react";
+import { useState } from "react";
+import ShowAllButton from "../components/buttons/show-all-button";
 import Checkbox from "../components/checkbox/checkbox";
 import SearchBar from "../components/search-bar/search-bar";
+import { categoryMap } from "../content/categories";
 import { Layout } from "../layout/layout";
-import ShowAllButton from "../components/buttons/show-all-button";
-import ArrowRightIcon from "../components/icons/arrow-right-icon";
-
-interface HeroCardProps {
-	title: string;
-	description: string;
-	image: string;
-	color: string;
-}
-
-export const HeroCard: React.FC<HeroCardProps> = ({
-	title,
-	description,
-	image,
-	color,
-}) => {
-	return (
-		<div className="h-56 flex flex-row gap-0 shadow-xl w-full border">
-			<div className={`${color} h-full w-[5%]`}></div>
-			<div className="w-[45%]">
-				<img src={image} alt="" className="object-cover h-full w-full" />
-			</div>
-			<div className="flex flex-col justify-between p-4 w-[50%]">
-				<div className="text-xl font-bold">{title}</div>
-				<div>{description}</div>
-				<button className="text-primary-blue text-left flex flex-row items-center justify-start gap-2">
-					<div>entdecken</div>
-					<ArrowRightIcon></ArrowRightIcon>
-				</button>
-			</div>
-		</div>
-	);
-};
+import { CategoryHeroCard } from "../components/categories/category-hero-card";
 
 export default function Index() {
+	const [showFreeOffersOnly, setShowFreeOffersOnly] = useState(false);
 	return (
 		<Layout>
 			<div className="max-w-4xl flex flex-col mx-auto">
@@ -44,41 +15,33 @@ export default function Index() {
 					<h1 className="text-2xl font-bold w-full flex flex-row justify-center my-8 text-center">
 						Was möchtest du unternehmen?
 					</h1>
-					<SearchBar value="" onSearch={() => {}}></SearchBar>
+					<SearchBar
+						value=""
+						onSearch={(s) => {
+							window.location.href = `/all-offers/?search=${s}&free=${showFreeOffersOnly ? "true" : "false"}`;
+						}}
+					/>
 					<Checkbox
 						id={"free-offers-only"}
 						title="Freier Entritt"
-						checked={true}
-						onCheck={() => {}}
+						checked={showFreeOffersOnly}
+						onCheck={() => {
+							setShowFreeOffersOnly(!showFreeOffersOnly);
+						}}
 					/>
 				</div>
 
 				<div className="w-full border-b border-separator mb-12 mt-2"></div>
 				<div className="w-full grid grid-cols-1 grid-rows-4 gap-x-8 gap-y-8 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-32 sm:gap-y-16 px-4 sm:px-0">
-					<HeroCard
-						title="Kultur"
-						description="Angebote fürs Museum, Theater, Konzerte und vieles mehr"
-						image="kultur.jpg"
-						color="bg-culture"
-					></HeroCard>
-					<HeroCard
-						title="Sport"
-						description="Angebote um körperlich fit zu bleiben oder den Sport zu feiern"
-						image="sport.jpg"
-						color="bg-sport"
-					></HeroCard>
-					<HeroCard
-						title="Beratung & Bildung"
-						description="Weiterbildungs- und Beratungsangebote"
-						image="bildung.jpg"
-						color="bg-education"
-					></HeroCard>
-					<HeroCard
-						title="Freizeit"
-						description="Angebote fürs Kino, Restaurants, Bars und Aktivitäten an der freien Luft"
-						image="freizeit.jpg"
-						color="bg-leisure"
-					></HeroCard>
+					{Object.entries(categoryMap)
+						.filter(([, category]) => category.isRendered)
+						.map(([key, category]) => (
+							<CategoryHeroCard
+								key={key}
+								identifier={key}
+								category={category}
+							/>
+						))}
 				</div>
 				<div className="w-full border-b border-separator mb-12 mt-12"></div>
 				<div className="w-full flex flex-row justify-end px-4 sm:px-0">
