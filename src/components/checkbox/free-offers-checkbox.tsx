ebe6b28eb5a2798d298ae8eddbf7dba91f591e36
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-09-12 13:40:54 +0200

    fix: show free offers checkbox

--- src/components/checkbox/free-offers-checkbox.tsx
+++ src/components/checkbox/free-offers-checkbox.tsx
@@ -9,12 +9,14 @@ const FreeOffersCheckbox: React.FC = () => {
 	const [searchParams] = useSearchParams();
 	const showFreeOffersOnly = (searchParams.get("free") ?? "false") === "true";
 	return (
-		<div className="flex flex-row items-center gap-3 py-2 text-lg font-bold">
+		<div
+			className="w-fit flex flex-row items-center gap-3 py-2 text-lg font-bold hover:cursor-pointer"
+			onClick={() => {
+				updateSearchParam("free", showFreeOffersOnly ? "false" : "true");
+			}}
+		>
 			<div
-				className={`${!showFreeOffersOnly && "border border-2 border-black"} w-5 h-5 flex flex-row items-center justify-center rounded-[2px] hover:cursor-pointer ${showFreeOffersOnly ? "bg-focus-blue" : ""}`}
-				onClick={() => {
-					updateSearchParam("free", showFreeOffersOnly ? "false" : "true");
-				}}
+				className={`${!showFreeOffersOnly && "border border-2 border-black"} w-5 h-5 flex flex-row items-center justify-center rounded-[2px]  ${showFreeOffersOnly ? "bg-focus-blue" : ""}`}
 			>
 				<CheckIcon></CheckIcon>
 			</div>
