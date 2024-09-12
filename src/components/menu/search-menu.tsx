3989d4b8f6e1dc32f561579072958637ea2494d6
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-09-12 15:17:58 +0200

    feat: search menu

---
+++ src/components/menu/search-menu.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { DrawerSearch } from "../drawer/drawer-search";
+import CloseIcon from "../icons/close-icon";
+import SearchBar from "../search-bar/search-bar";
+
+interface SearchMenuProps {
+	isOpen: boolean;
+	close: () => void;
+}
+
+const SearchMenu: React.FC<SearchMenuProps> = ({ isOpen, close }) => {
+	return (
+		<DrawerSearch isOpen={isOpen} close={() => close()}>
+			<div className="flex flex-col gap-4 px-6 py-4 text-base">
+				<div className="flex flex-row items-center justify-end  mb-6 mt-4">
+					<button onClick={() => close()}>
+						<CloseIcon></CloseIcon>
+					</button>
+				</div>
+			</div>
+			<div className="flex flex-row justify-center">
+				<div className="w-full px-4 md:w-[50%] flex flex-col gap-2 mt-[10vh] md:mt-[30vh]">
+					<h1 className="font-bold">Suche auf der Internetseite</h1>
+					<SearchBar
+						onSubmitOverride={(search) => {
+							window.location.href = `/all-offers/?search=${search}`;
+						}}
+					></SearchBar>
+				</div>
+			</div>
+		</DrawerSearch>
+	);
+};
+
+export default SearchMenu;
