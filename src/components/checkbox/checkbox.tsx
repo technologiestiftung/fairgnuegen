ec7b94e8d3849e13021c71f0c8a2eecc8707b2aa
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-09-12 13:40:12 +0200

    fix: whole width of checkbox clickable

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -9,12 +9,14 @@ interface CheckboxProps {
 
 const Checkbox: React.FC<CheckboxProps> = ({ label, isChecked, onChange }) => {
 	return (
-		<div className="flex flex-row items-start gap-3 py-2 text-lg font-bold">
+		<div
+			className="w-full flex flex-row items-start gap-3 py-2 text-lg font-bold  hover:cursor-pointer"
+			onClick={() => {
+				onChange();
+			}}
+		>
 			<div
-				className={`${!isChecked && "border border-2 border-black"} mt-1 w-5 h-5 min-w-5 min-h-5 flex flex-row items-center justify-center rounded-[2px] hover:cursor-pointer ${isChecked ? "bg-focus-blue" : ""}`}
-				onClick={() => {
-					onChange();
-				}}
+				className={`${!isChecked && "border border-2 border-black"} mt-1 w-5 h-5 min-w-5 min-h-5 flex flex-row items-center justify-center rounded-[2px] ${isChecked ? "bg-focus-blue" : ""}`}
 			>
 				<CheckIcon></CheckIcon>
 			</div>
