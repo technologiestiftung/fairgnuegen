4a5ac9e6be92bd1848266ea8a3d51b3e522c601a
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-09-16 11:09:03 +0200

    feat: render all markers

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -2,31 +2,42 @@ import { useEffect, useRef } from "react";
 import { Layout } from "../../layout/layout";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { useFilteredAndSortedOffers } from "../../hooks/use-filtered-and-sorted-offers";
 
 export default function Index() {
+	const { filteredAndSortedOffers } = useFilteredAndSortedOffers();
 	const mapRef = useRef<HTMLDivElement>(null);
+	const map = useRef<maplibregl.Map | null>(null);
 
 	useEffect(() => {
-		const map = new maplibregl.Map({
-			container: "map", // container id
+		const initMap = new maplibregl.Map({
+			container: "map",
 			style:
 				"https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_col.json", // style URL
-			center: [13.404954, 52.520008], // starting position [lng, lat]
-			zoom: 15, // starting zoom
+			center: [13.404954, 52.520008],
+			zoom: 11,
 		});
-
-		const popup = new maplibregl.Popup({ offset: 25 }).setHTML(`
-    <div class="bg-white border-2 border-black p-4 rounded-lg">
-      <h1 class="text-lg text-blue-600">Custom Popup</h1>
-      <p class="text-sm text-gray-500">This is a custom-styled popup using Tailwind CSS.</p>
-    </div>
-  `);
-
-		let marker = new maplibregl.Marker()
-			.setLngLat([13.404954, 52.520008])
-			.addTo(map)
-			.setPopup(popup);
+		map.current = initMap;
 	}, []);
+
+	useEffect(() => {
+		if (filteredAndSortedOffers && map.current) {
+			filteredAndSortedOffers.forEach((offer) => {
+				const popup = new maplibregl.Popup({ offset: 25, closeButton: false })
+					.setHTML(`
+		<div class="bg-white p-2 rounded-lg">
+		  <h1 class="text-lg">${offer.provider}</h1>
+		  <p class="text-sm">${offer.providerDescription}</p>
+		</div>
+	  `);
+
+				new maplibregl.Marker()
+					.setLngLat([offer.x, offer.y])
+					.addTo(map.current)
+					.setPopup(popup);
+			});
+		}
+	}, [filteredAndSortedOffers, map]);
 
 	return (
 		<Layout>
