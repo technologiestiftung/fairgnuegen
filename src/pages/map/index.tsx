8abe8578902160e1d235e5ba7f9348411b26a89a
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-09-17 14:01:14 +0200

    feat: basic map popup (#33)
    * feat: map popup
    
    * fix: use effects
    
    * chore: refactor
    
    * feat: refactoring, cleanup

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,22 +1,43 @@
 import "maplibre-gl/dist/maplibre-gl.css";
-// import OfferDetail from "../../components/offer/offer-detail";
+import { useEffect, useRef, useState } from "react";
+import OfferPopup from "../../components/offer/offer-popup";
 import { useMap } from "../../hooks/use-map";
 import { useMapInteraction } from "../../hooks/use-map-interaction";
 import { Layout } from "../../layout/layout";
 
 export default function Index() {
 	const { mapRef, isMapLoading } = useMap();
-	useMapInteraction(mapRef);
+	const { selectedOffer } = useMapInteraction(mapRef);
+	const popupRef = useRef<HTMLDivElement | null>(null);
+	const [topAnchor, setTopAnchor] = useState(0);
+
+	const mapVerticalBaseline = 600 / 2;
+	const iconOffset = 30;
+
+	useEffect(() => {
+		if (popupRef.current) {
+			setTopAnchor(
+				mapVerticalBaseline - popupRef.current.clientHeight - iconOffset,
+			);
+		}
+	}, [selectedOffer, popupRef]);
 
 	return (
 		<Layout>
 			<div className="grid grid-cols-1 grid-rows-1 relative">
-				<div id="map" className="row-start-1 col-start-1 w-full h-[70vh]" />
-				{/* {selectedOffer && (
-					<div className="absolute left-0 top-0 w-[500px] p-4 bg-white">
-						<OfferDetail offer={selectedOffer}></OfferDetail>
+				<div id="map" className="row-start-1 col-start-1 w-full h-[600px]" />
+
+				{selectedOffer && (
+					<div
+						className={`absolute left-0 right-0 w-[500px] mx-auto`}
+						ref={popupRef}
+						style={{
+							top: `${topAnchor}px`,
+						}}
+					>
+						<OfferPopup offer={selectedOffer} />
 					</div>
-				)} */}
+				)}
 				{isMapLoading && (
 					<div
 						id="map"
