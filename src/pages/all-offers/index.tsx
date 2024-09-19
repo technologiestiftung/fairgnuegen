19ea7af32c0aa7e8be951f7a20c396c41857ac6b
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-09-19 11:41:20 +0200

    fix: cleanup

--- src/pages/all-offers/index.tsx
+++ src/pages/all-offers/index.tsx
@@ -43,11 +43,9 @@ export default function Index() {
 
 				<div className="max-w-3xl mx-auto flex flex-col">
 					<div className="mx-4 lg:mx-0">
-						<div className="flex flex-col mb-10">
+						<div className="flex flex-col gap-2 mb-10">
 							<SearchBar />
-							<div className="pt-2">
-								<FreeOffersCheckbox />
-							</div>
+							<FreeOffersCheckbox />
 						</div>
 						<div className="flex flex-row w-full justify-between mb-6">
 							<SortButton></SortButton>
