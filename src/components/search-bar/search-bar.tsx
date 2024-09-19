21f1eae5cdb055a2dac37babd8e13866c21fc847
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-09-19 09:26:13 +0200

    fix: search not rounded, make invisible input w-0 (#41)

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -30,7 +30,7 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSubmitOverride }) => {
 			<input
 				value={search}
 				type="text"
-				className="relative pl-8 pr-20 row-start-1 col-start-1 w-full h-[42px] border-2 border-black px-4 focus:outline-none focus:border-focus-blue focus:shadow-default"
+				className="relative pl-8 pr-20 row-start-1 col-start-1 w-full h-[42px] border-2 border-black px-4 focus:outline-none focus:border-focus-blue focus:shadow-default rounded-none"
 				placeholder="Suchbegriff eingeben"
 				onChange={(e) => setSearch(e.target.value)}
 				onFocus={() => {
