dfe4193b9c3538d516a1877ed69d578cddee9528
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-09-23 14:28:33 +0200

    feat: icon size grows on click (#56)
    * feat: increase icon size on click
    
    * fix: load markers async

--- src/hooks/use-map-interaction.tsx
+++ src/hooks/use-map-interaction.tsx
@@ -13,6 +13,43 @@ export function useMapInteraction(
 			return;
 		}
 
+		if (!map.current.isStyleLoaded()) {
+			return;
+		}
+
+		if (selectedOffer) {
+			map.current.setLayoutProperty("unclustered-point", "icon-size", [
+				"interpolate",
+				["linear"],
+				["zoom"],
+				0,
+				["match", ["get", "id"], selectedOffer.path, 0.15, 0.1],
+				12,
+				["match", ["get", "id"], selectedOffer.path, 0.75, 0.5],
+				22,
+				["match", ["get", "id"], selectedOffer.path, 1.5, 1.0],
+			]);
+			return;
+		}
+
+		map.current.setLayoutProperty("unclustered-point", "icon-size", [
+			"interpolate",
+			["linear"],
+			["zoom"],
+			0,
+			0.1,
+			12,
+			0.5,
+			22,
+			1.0,
+		]);
+	}, [map, selectedOffer]);
+
+	useEffect(() => {
+		if (!map.current) {
+			return;
+		}
+
 		map.current.on("mouseenter", "unclustered-point", () => {
 			if (!map.current) {
 				return;
@@ -67,9 +104,14 @@ export function useMapInteraction(
 			}
 		});
 
-		map.current.on("dragstart", () => {
-			setSelectedOffer(null);
-		});
+		if (window.innerWidth > 768) {
+			map.current.on("dragstart", () => {
+				if (!map.current) {
+					return;
+				}
+				setSelectedOffer(null);
+			});
+		}
 	}, []);
 
 	return { selectedOffer };
