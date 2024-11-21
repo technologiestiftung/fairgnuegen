3533245bc46676607b6a8229eb71fcacb990066a
aeschi <56318362+aeschi@users.noreply.github.com>
2024-11-21 09:39:34 +0100

    fix: align bright and dark footer (#117)
    * fix: align bright and dark footer
    
    * Delete tsconfig.app.tsbuildinfo
    
    * Delete tsconfig.node.tsbuildinfo
    
    * fix: alignment

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -58,21 +58,23 @@ export function Footer() {
 	return (
 		<div>
 			<div className="flex items-start justify-between w-full bg-berlin-grey-light desktop:p-6">
-				<div className="flex flex-col desktop:flex-row w-full desktop:gap-x-10 desktop:pl-28">
-					<div className="desktop:hidden self-end">
+				<div className="desktop:pl-[5%] flex items-start justify-between w-full">
+					<div className="flex flex-col desktop:flex-row desktop:max-w-[210px]">
+						<div className="desktop:hidden self-end">
+							<ScrollToTopButton />
+						</div>
+
+						{upperFooterColumns.map((column) => (
+							<FooterLightColumn
+								key={column.title}
+								title={column.title}
+								links={column.links}
+							/>
+						))}
+					</div>
+					<div className="hidden desktop:block">
 						<ScrollToTopButton />
 					</div>
-
-					{upperFooterColumns.map((column) => (
-						<FooterLightColumn
-							key={column.title}
-							title={column.title}
-							links={column.links}
-						/>
-					))}
-				</div>
-				<div className="hidden desktop:block">
-					<ScrollToTopButton />
 				</div>
 			</div>
 
