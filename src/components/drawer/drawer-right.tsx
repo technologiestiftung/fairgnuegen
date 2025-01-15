9b16a37d69ccecfd540846b74251e11041f39d1b
Raphael Arce <raphael.arce@ts.berlin>
2025-01-15 16:28:32 +0100

    feat: polish menu sidebar
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/components/drawer/drawer-right.tsx
+++ src/components/drawer/drawer-right.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useOnEscape } from "../../hooks/use-on-escape";
 
 interface DrawerProps {
 	isOpen: boolean;
@@ -7,6 +8,8 @@ interface DrawerProps {
 }
 
 export function DrawerRight({ isOpen, close, children }: DrawerProps) {
+	useOnEscape(close);
+
 	return (
 		<div
 			className={`overflow-x-hidden overflow-y-hidden fixed top-0 right-0 z-20 w-screen h-screen pointer-events-none`}
@@ -16,7 +19,7 @@ export function DrawerRight({ isOpen, close, children }: DrawerProps) {
 				className={`${isOpen ? "opacity-100" : "opacity-0"} transition absolute top-0 left-0 w-screen h-screen bg-black bg-opacity-40 ${isOpen ? "pointer-events-auto" : "pointer-events-none"}`}
 			/>
 			<div
-				className={`${isOpen ? "translate-x-0" : "translate-x-[90vw] md:translate-x-[500px]"} transition absolute top-0 right-0  h-full bg-white w-[90vw] md:w-[500px] overflow-y-scroll pointer-events-auto`}
+				className={`${isOpen ? "translate-x-0" : "translate-x-[90vw] md:translate-x-[560px]"} transition absolute top-0 right-0  h-full bg-white w-[90vw] md:w-[560px] overflow-y-scroll pointer-events-auto`}
 				onClick={(event) => event.stopPropagation()}
 			>
 				{children}
