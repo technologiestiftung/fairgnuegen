7406823d8b44ab4c109842d3a801f3d6feef4918
Raphael Arce <raphael.arce@ts.berlin>
2025-01-28 13:14:32 +0100

    feat: make last chevron in breadcrumbs black
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/components/icons/chevron-right.tsx
+++ src/components/icons/chevron-right.tsx
@@ -1,15 +1,16 @@
-export function ChevronRight() {
+export function ChevronRight({ className }: { className?: string }) {
 	return (
 		<svg
 			width="6"
 			height="10"
 			viewBox="0 0 6 10"
 			fill="none"
+			className={className}
 			xmlns="http://www.w3.org/2000/svg"
 		>
 			<path
 				d="M1 8.5L5 5L1 1.5"
-				stroke="#777777"
+				stroke="currentColor"
 				strokeWidth="1.25"
 				strokeLinecap="round"
 			/>
