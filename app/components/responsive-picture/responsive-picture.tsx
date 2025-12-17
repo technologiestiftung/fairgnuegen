1c776fbe0c0a37dfae95fd04f57276bb70d928af
aeschi <56318362+aeschi@users.noreply.github.com>
2025-12-17 16:07:25 +0100

    feat: move credits to all offers

--- app/components/responsive-picture/responsive-picture.tsx
+++ app/components/responsive-picture/responsive-picture.tsx
@@ -1,6 +1,3 @@
-import { useLanguage } from "~/hooks/use-language.tsx";
-import { useI18n } from "~/i18n/use-i18n.tsx";
-
 type ResponsivePictureProps = {
 	src: string;
 	alt: string;
@@ -14,14 +11,11 @@ export function ResponsivePicture({
 	loading,
 	className,
 }: ResponsivePictureProps) {
-	const language = useLanguage();
-	const i18n = useI18n(language);
-
 	const sizes = [640, 1024];
 	const defaultSize = 1920;
 
 	return (
-		<picture className="relative">
+		<picture>
 			{sizes.map((size) => (
 				<source
 					key={size}
@@ -40,9 +34,6 @@ export function ResponsivePicture({
 				height={defaultSize / 1.5}
 				className={className}
 			/>
-			<p className="text-[11px] text-white/40 absolute bottom-[5px] right-[7px] z-10">
-				{i18n["categories.all.imageCredit"]}
-			</p>
 		</picture>
 	);
 }
