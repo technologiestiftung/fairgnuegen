c33e9ed1a94059aa675018235bdaf778e7b15501
Rapha <raphael.arce@ts.berlin>
2026-01-20 12:17:48 +0100

    feat: replace divs with spans inside of button

--- app/components/buttons/show-filtered-button.tsx
+++ app/components/buttons/show-filtered-button.tsx
@@ -17,14 +17,14 @@ const ShowFilteredButton: React.FC<ShowFilteredButtonProps> = ({ onClick }) => {
 			className="h-[43px] border-black border-2 opacity-100 flex justify-center items-center w-[250px]"
 			onClick={onClick}
 		>
-			<div className="flex flex-row justify-end items-center h-full w-full hover:bg-berlin-grey-light">
-				<div className="w-[207px] flex flex-row items-center justify-center font-normal">
+			<span className="flex flex-row justify-end items-center h-full w-full hover:bg-berlin-grey-light">
+				<span className="w-[207px] flex flex-row items-center justify-center font-normal">
 					{filteredAndSortedOffers.length} {i18n["filter.showOffers"]}
-				</div>
-				<div className="h-full bg-primary-red w-[43px] flex flex-row items-center justify-center border-l-black border-l-2">
+				</span>
+				<span className="h-full bg-primary-red w-[43px] flex flex-row items-center justify-center border-l-black border-l-2">
 					<ArrowRightIcon color="text-white"></ArrowRightIcon>
-				</div>
-			</div>
+				</span>
+			</span>
 		</button>
 	);
 };
